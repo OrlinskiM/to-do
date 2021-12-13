5ff4b1b31c420fbf70eb3df63083f4677b7bf423
OrlinskiM <matiorlik@gmail.com>
2021-12-13 18:09:40 +0100

    test

--- src/app/list.service.ts
+++ src/app/list.service.ts
@@ -6,4 +6,32 @@ import { Injectable } from '@angular/core';
 export class ListService {
 
   constructor() { }
+
+  values = '';
+  text = "";
+  list: string[] = [];
+
+  test(ev: KeyboardEvent) {
+  this.values = (ev.target as HTMLInputElement).value;
+  }
+
+  onAdd(){
+    this.list.push(this.text);
+    localStorage.setItem("to-do", JSON.stringify(this.list));
+    this.text = "";
+  }
+
+  deleteTask(inputText : string){
+    const index = this.list.indexOf(inputText);
+    if (index !== - 1){
+      this.list.splice(index, 1);
+      localStorage.setItem("to-do", JSON.stringify(this.list));
+    }
+  }
+
+
+  ngOnInit(): void {
+    const memList = localStorage.getItem("to-do");
+    if(memList) this.list = JSON.parse(memList);
+  }
 }
